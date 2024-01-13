a7c6415fcc4616302e086e8bdee65d683b578634
Fatih <Fatih@DESKTOP-CRO1V9C>
2024-01-13 22:21:38 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/Discount.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Amount { get; set; }
+        public int Description { get; set; }
+        public int ImageUrl { get; set; }
+    }
+}
